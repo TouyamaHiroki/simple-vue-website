c5a40ea283b494e62ee07e36feca87bf5b173286
TouyamaHiroki <h-touyama@h-technology.jp>
2023-03-27 15:17:49 +0900

    Header-Footer Set

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <AppHeader/>
+    <AppFooter/>
+  </v-app>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer
+  dark
+  absolute
+  >
+  <div class="footer">
+    Copyright test 2023. App rights reserved
+  </div>
+  </v-footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  margin: auto;
+}
+</style>
